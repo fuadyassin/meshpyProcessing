fd3b1a8cb1ea15e0710dc0825f07f71621de24ea
fuadyassin <fuad.yassin@usask.ca>
2024-08-30 11:24:50 -0600

    Update gsde_soil.py

--- MESHpyPreProcessing/gsde_soil.py
+++ MESHpyPreProcessing/gsde_soil.py
@@ -7,24 +7,6 @@ import xarray as xr
 
 class GSDESoil:
     def __init__(self, directory, input_basin, output_shapefile):
-        """
-        Initialize the GSDESoil class with directory, input basin, and output shapefile.
-        
-        Code:
-            def __init__(self, directory, input_basin, output_shapefile):
-                self.directory = directory
-                self.input_basin = input_basin
-                self.output_shapefile = output_shapefile
-                self.file_paths = []
-                self.gsde_df = pd.DataFrame()
-                self.merged_gdf = gpd.GeoDataFrame()
-                self.weights_used = []
-                self.mesh_intervals = []
-                self.lon = []
-                self.lat = []
-                self.segid = []
-                self.num_soil_lyrs = 0
-        """
         self.directory = directory
         self.input_basin = input_basin
         self.output_shapefile = output_shapefile
@@ -38,83 +20,50 @@ class GSDESoil:
         self.segid = []
         self.num_soil_lyrs = 0
 
-    def load_data(self, file_names):
+    def load_data(self, file_names, search_replace_dict=None, suffix_list=None):
         """
-        Load data from multiple CSV files and merge them into a single DataFrame.
-        
-        Code:
-            def load_data(self, file_names):
-                self.file_paths = [os.path.join(self.directory, filename) for filename in file_names]
-                self.gsde_df = self.load_and_merge_files(self.file_paths)
+        Load data from multiple CSV files, apply renaming/suffixing, and merge into a single DataFrame.
+        Parameters:
+            - file_names: List of CSV file names to be loaded.
+            - search_replace_dict: Dictionary with file name as key and (search_list, replace_list) tuple as value.
+            - suffix_list: List of suffixes to be appended to column names for each file.
         """
         self.file_paths = [os.path.join(self.directory, filename) for filename in file_names]
-        self.gsde_df = self.load_and_merge_files(self.file_paths)
+        self.gsde_df = self.load_and_merge_files(self.file_paths, search_replace_dict, suffix_list)
 
     @staticmethod
-    def load_and_merge_files(file_list, key='COMID'):
+    def load_and_merge_files(file_list, search_replace_dict=None, suffix_list=None, key='COMID'):
         """
         Load and merge multiple CSV files on the specified key.
-        
-        Code:
-            @staticmethod
-            def load_and_merge_files(file_list, key='COMID'):
-                dfs = [pd.read_csv(fp) for fp in file_list]
-                return reduce(lambda left, right: pd.merge(left, right, on=key, how='outer'), dfs)
+        Apply renaming/suffixing during the reading of each file.
         """
-        dfs = [pd.read_csv(fp) for fp in file_list]
+        dfs = []
+        for i, fp in enumerate(file_list):
+            df = pd.read_csv(fp)
+            file_name = os.path.basename(fp)
+            
+            # Apply search and replace for column names if specified
+            if search_replace_dict and file_name in search_replace_dict:
+                search_list, replace_list = search_replace_dict[file_name]
+                for search, replace in zip(search_list, replace_list):
+                    df.columns = [col.replace(search, str(replace)) for col in df.columns]
+            
+            # Remove periods from column names
+            df.columns = [col.replace('.', '') for col in df.columns]
+            
+            # Optionally append a suffix to the column names
+            if suffix_list and suffix_list[i]:
+                suffix = suffix_list[i]
+                df.columns = [f"{col}_{suffix}" if col != key else col for col in df.columns]
+            
+            dfs.append(df)
+        
+        # Merge all the dataframes on the specified key
         return reduce(lambda left, right: pd.merge(left, right, on=key, how='outer'), dfs)
-
-    def clean_column_names(self, search_list, replace_list):
-        """
-        Clean column names by replacing search strings with corresponding replace strings.
-        
-        Code:
-            def clean_column_names(self, search_list, replace_list):
-                new_columns = []
-                for column in self.gsde_df.columns:
-                    for search, replace in zip(search_list, replace_list):
-                        if search in column:
-                            column = column.replace(search, replace)
-                    column = column.replace('.', '')  # Remove periods from column names
-                    new_columns.append(column)
-                self.gsde_df.columns = new_columns
-        """
-        new_columns = []
-        for column in self.gsde_df.columns:
-            for search, replace in zip(search_list, replace_list):
-                if search in column:
-                    column = column.replace(search, replace)
-            column = column.replace('.', '')  # Remove periods from column names
-            new_columns.append(column)
-        self.gsde_df.columns = new_columns
 
     def fill_and_clean_data(self, exclude_cols=['COMID'], exclude_patterns=['OC', 'BD', 'BDRICM', 'BDTICM'], max_val=100):
         """
         Fill and clean data, replacing values greater than max_val with NaN and forward/backward filling.
-        
-        Code:
-            def fill_and_clean_data(self, exclude_cols=['COMID'], exclude_patterns=['OC', 'BD', 'BDRICM', 'BDTICM'], max_val=100):
-                for col in self.gsde_df.columns:
-                    if col not in exclude_cols:
-                        if 'OC' in col:
-                            self.gsde_df.loc[self.gsde_df[col]*1.72*0.01 > max_val, col] = np.nan                
-                    if col not in exclude_cols:
-                        if not any(pattern in col for pattern in exclude_patterns):
-                            self.gsde_df.loc[self.gsde_df[col] > max_val, col] = np.nan
-                
-                    if 'BDRICM' in col or 'BDTICM' in col:
-                        self.gsde_df.loc[self.gsde_df[col] == 9999.0, col] = np.nan
-                        self.gsde_df[col] = self.gsde_df[col] / 100.0
-                        if 'BDRICM' in col:
-                            self.gsde_df.loc[self.gsde_df[col] > 2.0, col] = 2.0
-                            self.gsde_df.loc[self.gsde_df[col] < 0.1, col] = 0.1
-                        if 'BDTICM' in col:
-                            self.gsde_df.loc[self.gsde_df[col] > 4.1, col] = 4.1
-                            self.gsde_df.loc[self.gsde_df[col] < 0.1, col] = 0.1
-                
-                self.gsde_df.sort_values('COMID', inplace=True)
-                self.gsde_df.fillna(method='ffill', inplace=True)
-                self.gsde_df.fillna(method='bfill', inplace=True)
         """
         for col in self.gsde_df.columns:
             if col not in exclude_cols:
@@ -138,23 +87,6 @@ class GSDESoil:
     def calculate_weights(self, gsde_intervals, mesh_intervals):
         """
         Calculate weights for different soil intervals.
-        
-        Code:
-            def calculate_weights(self, gsde_intervals, mesh_intervals):
-                self.mesh_intervals = mesh_intervals
-                self.num_soil_lyrs = len(mesh_intervals)
-                weights_used = []
-                for mesh_interval in mesh_intervals:
-                    start, end = mesh_interval
-                    weights = []
-                    for gsde_interval in gsde_intervals:
-                        gsde_start, gsde_end = gsde_interval
-                        overlap_start = max(start, gsde_start)
-                        overlap_end = min(end, gsde_end)
-                        weight = (overlap_end - overlap_start) / (end - start) if overlap_start < overlap_end else 0
-                        weights.append(weight)
-                    weights_used.append([w / sum(weights) for w in weights if sum(weights) > 0])
-                self.weights_used = weights_used
         """
         self.mesh_intervals = mesh_intervals
         self.num_soil_lyrs = len(mesh_intervals)
@@ -174,17 +106,6 @@ class GSDESoil:
     def calculate_mesh_values(self, column_names):
         """
         Calculate mesh values by applying weights to soil property columns.
-        
-        Code:
-            def calculate_mesh_values(self, column_names):
-                for prop, cols in column_names.items():
-                    extracted_data = self.gsde_df[cols]
-                    weights_array = np.array(self.weights_used).T
-                    mesh_values = np.dot(extracted_data, weights_array)
-                    if prop == 'OC':
-                        mesh_values *= 0.01 * 1.72
-                    for i, mesh_col in enumerate([f'mesh{prop}{j+1}' for j in range(len(self.mesh_intervals))]):
-                        self.gsde_df[mesh_col] = mesh_values[:, i]
         """
         for prop, cols in column_names.items():
             extracted_data = self.gsde_df[cols]
@@ -198,13 +119,6 @@ class GSDESoil:
     def merge_and_save_shapefile(self):
         """
         Merge soil data with a shapefile and save the result.
-        
-        Code:
-            def merge_and_save_shapefile(self):
-                gdf = gpd.read_file(self.input_basin).to_crs(epsg=4326)
-                gdf['COMID'] = gdf['COMID'].astype(int)
-                self.merged_gdf = gdf.merge(self.gsde_df, on='COMID', how='left')
-                self.merged_gdf.to_file(self.output_shapefile)
         """
         gdf = gpd.read_file(self.input_basin).to_crs(epsg=4326)
         gdf['COMID'] = gdf['COMID'].astype(int)
@@ -214,14 +128,6 @@ class GSDESoil:
     def set_coordinates(self, input_ddb):
         """
         Set longitude and latitude values from a NetCDF drainage database.
-        
-        Code:
-            def set_coordinates(self, input_ddb):
-                db = xr.open_dataset(input_ddb)
-                self.lon = db.variables['lon'].values
-                self.lat = db.variables['lat'].values
-                self.segid = db.variables['subbasin'].values
-                db.close()
         """
         db = xr.open_dataset(input_ddb)
         self.lon = db.variables['lon'].values
